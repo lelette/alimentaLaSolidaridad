0fc1d527d4427c51dfdb33f3a3bea14221df11cb
Miguel Mendoza <miguel@debian.miguel>
2017-10-29 19:15:00 -0400

    Actualizando codigo para el cambio de imagen de perfil

--- assets/js/config.router.js
+++ assets/js/config.router.js
@@ -6,7 +6,7 @@
 
 
  // Prefijo de URL para el Core de Servicios
- var API_URL = 'http://192.168.3.166:9002/';
+ var API_URL = 'http://192.168.2.28:9002/';
  //var API_URL = 'http://192.168.3.66:8002/';
 
  function apiInterceptor($q, $cookies) {
@@ -187,11 +187,10 @@ angular.module('app')
                 'js/controllers/app/profile.js'
               ])
           })
-          .state('app.page.imageChange', {
+          .state('app.page.profile.imageChange', {
             url: '/changeImage',
             templateUrl: 'templates/app/profile/changeImage.html',
             resolve: load([
-              'js/controllers/app/imageChange.js',
               'ngImgCrop',
               'filestyle',
               'angularFileUpload'
