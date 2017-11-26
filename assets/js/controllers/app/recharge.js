36403261e07177b4afef1115caee282c4ffbffc4
Samil Sanchez <samil_alex@hotmail.com>
2017-11-26 11:59:44 -0400

    actualizando archivos

--- assets/js/controllers/app/recharge.js
+++ assets/js/controllers/app/recharge.js
@@ -245,6 +245,10 @@ app.controller('ReloadController',
   ['$rootScope', '$scope', '$http', '$state', 'Recharge', '$translate', '$stateParams',
   function($rootScope,  $scope, $http, $state, Recharge, $translate, $stateParams) {
     console.log('Recharge', Recharge);
+
+    $scope.btnClass = "btn-off";
+    $scope.error_msj_oferta = false;
+    $scope.cart = [];
     $scope.datos = {
       cod: 'Pais',
       contrato: '',
@@ -273,42 +277,37 @@ app.controller('ReloadController',
   //    $scope.datos.contrato = $stateParams.contrato;
   //  }
 
-    // Consulta las tarjetas afiliadas del usuario
-    $scope.loader='mostrar';
-    $scope.cuerpo='ocultar';
-    $http.get('platform/stripe/getCards')
-      .then(function(response){
-        $scope.loader='ocultar';
-        $scope.cuerpo='mostrar';
-        $scope.cards = response.data.data;
-      }, function(error){
-        $scope.loader='ocultar';
-        $scope.cuerpo='mostrar';
-      });
-
-    $http.get('plataform/sales/getshoppingCart')
-    .then(function (res) {
-      console.log('Hola mundo!!!! ',res.data);
-    }, function (error) {
-      console.log('Hola ERROR!!!! ',error.data);
+  // Consulta las tarjetas afiliadas del usuario
+  $scope.loader='mostrar';
+  $scope.cuerpo='ocultar';
+  $http.get('platform/stripe/getCards')
+    .then(function(response){
+      $scope.loader='ocultar';
+      $scope.cuerpo='mostrar';
+      $scope.cards = response.data.data;
+    }, function(error){
+      $scope.loader='ocultar';
+      $scope.cuerpo='mostrar';
     });
 
-    /*************************************************************************************************
-    *    function     :: Funcion que permite añadir con cual tarjeta afiliada se va realizar el pago *
-    *    @description :: Permite saber cual tarjeta afiliada fue seleccionada mediante el checkbox   *
-    *    @autor       :: Javier Stifano <jstifano@transamovil.com>                                   *
-    **************************************************************************************************/
+  $http.get('plataform/sales/getshoppingCart')
+  .then(function (res) {
+    console.log('Hola mundo!!!! ',res.data);
+  }, function (error) {
+    console.log('Hola ERROR!!!! ',error.data);
+  });
 
-<<<<<<< HEAD
-    $scope.updateSelected = function(token){
-      $scope.cardSelected = token;
-    }
+  /*************************************************************************************************
+  *    function     :: Funcion que permite añadir con cual tarjeta afiliada se va realizar el pago *
+  *    @description :: Permite saber cual tarjeta afiliada fue seleccionada mediante el checkbox   *
+  *    @autor       :: Javier Stifano <jstifano@transamovil.com>                                   *
+  **************************************************************************************************/
 
-     $scope.recharge = function () {
-       $scope.loader='mostrar';
-       $scope.cuerpo='ocultar';
-       $http.post('plataform/sales/recharge', Recharge.info).then(function(response) {
-=======
+  $scope.updateSelected = function(token){
+    $scope.cardSelected = token;
+  }
+
+
   $scope.agregarAlCarrito = function () {
     $http.post('plataform/sales/shoppingCartRegister', Recharge.info.oferta)
     .then(function (res) {
@@ -405,127 +404,61 @@ app.controller('ReloadController',
             "currency": "EUR",
        }).then(function(res){
          $scope.error_msj_oferta = false;
->>>>>>> feature/Ajustando_flujo_de_recarga_frontend
          $scope.loader='ocultar';
          $scope.cuerpo='mostrar';
-         $scope.recharge = response.data.recharge;
-         $scope.$emit('$resetAjax');
-       }, function(res) {
-         $scope.loader='ocultar';
-         $scope.cuerpo='mostrar';
-         $scope.$emit('$resetAjax');
-         $scope.$emit('$errorAjax',res.data);
-       });
-     }
-
-     if (Recharge.info.pais && Recharge.info.pais.codigo && Recharge.info.pais.numero && Recharge.info.pais.url
-       && Recharge.info.pais.operadora && Recharge.info.ofertas) {
-
+         var ofertas = res.data;
+         var operadora = ofertas.operadora;
+         Recharge.info.ofertas = ofertas;
+         Recharge.info.pais = {
+           codigo: cod,
+           numero: numero,
+           operadora: operadora
+         }
+        $scope.datos.operadora = operadora;
         $scope.ofertas = Recharge.info.ofertas;
-        $scope.pais.cod = Recharge.info.pais.codigo;
-        $scope.pais.url = Recharge.info.pais.url;
-        $scope.datos.contrato = Recharge.info.pais.numero
-        $scope.datos.operadora = Recharge.info.pais.operadora;
-        $scope.showCountry = true;
-        $scope.showOffers = true;
-        $scope.showOperator = true;
+        $scope.showOffers = true; $scope.showOperator = true;
+      }, function(res){
         $scope.loader = 'ocultar';
         $scope.cuerpo = 'mostrar';
+        $scope.showOffers = false;
+        $scope.error_msj_oferta = true;
+        console.log(res);
+        /*$scope.$emit('$resetAjax');
+        $scope.$emit('$errorAjax',res.data);*/
+      });
+    }
+   }
 
-     }else {
-       $http.get('plataform/countries').then(function(response) {
-         $scope.countries = response.data.paises;
-         $scope.$emit('$resetAjax');
-         $scope.loader = 'ocultar';
-         $scope.cuerpo = 'mostrar';
-       }, function(res) {
-         $scope.loader = 'ocultar';
-         $scope.cuerpo = 'mostrar';
-         $scope.$emit('$resetAjax');
-         $scope.$emit('$errorAjax',res.data);
-       });
+   /*********************************************************************************************
+   *    function     :: Funcion que permite saber que tipo de tarjeta va utilizar dinámicamente *
+   *    @description :: Permite adivinar que tipo de TDC es con los primeros dígitos de la TDC  *
+   *    @autor       :: Javier Stifano <jstifano@transamovil.com>                               *
+   **********************************************************************************************/
+
+   $scope.showType = function(card){
+
+     if(!card){
+       $scope.cardType = 'Not found';
+       return $scope.cardType;
      }
-
-     $scope.countrySelected = function (country) {
-       $scope.showCountry = true;
-       $scope.pais = {
-         url: 'images/banderas/'+country.name+'.png',
-         cod: '+'+country.phone_code
-       };
-       $scope.datos.cod = country.phone_code;
-       Recharge.info.pais.codigo = country.phone_code;
-     };
-
-     $scope.obtenerOfertas = function (){
-       $scope.showOffers = false;
-       $scope.loader = 'mostrar';
-       $scope.cuerpo = 'ocultar';
-
-       var numero = $scope.datos.contrato;
-       var cod = $scope.datos.cod;
-
-       if (Recharge.info.pais && Recharge.info.pais.codigo) cod = Recharge.info.pais.codigo;
-
-      if (cod && numero) {
-        $http.post('plataform/offers',{
-              // Telefono de reales --> España "34912509849" ; Argentina "5491127184499"
-              "phone":  cod+numero,
-              "currency": "EUR",
-         }).then(function(res){
-           $scope.loader='ocultar';
-           $scope.cuerpo='mostrar';
-           var ofertas = res.data;
-           var operadora = ofertas.operadora;
-           Recharge.info.ofertas = ofertas;
-           Recharge.info.pais = {
-             codigo: cod,
-             numero: numero,
-             operadora: operadora
-           }
-          $scope.datos.operadora = operadora;
-          $scope.ofertas = Recharge.info.ofertas;
-          $scope.showOffers = true; $scope.showOperator = true;
-        }, function(res){
-          $scope.loader = 'ocultar';
-          $scope.cuerpo = 'mostrar';
-          $scope.showOffers = false;
-          console.log(res);
-          $scope.$emit('$resetAjax');
-          $scope.$emit('$errorAjax',res.data);
-        });
-      }
+     else if(card[0] == '4'){
+       $scope.cardType = 'Visa';
+       return $scope.cardType;
      }
-
-     /*********************************************************************************************
-     *    function     :: Funcion que permite saber que tipo de tarjeta va utilizar dinámicamente *
-     *    @description :: Permite adivinar que tipo de TDC es con los primeros dígitos de la TDC  *
-     *    @autor       :: Javier Stifano <jstifano@transamovil.com>                               *
-     **********************************************************************************************/
-
-     $scope.showType = function(card){
-
-       if(!card){
-         $scope.cardType = 'Not found';
-         return $scope.cardType;
-       }
-       else if(card[0] == '4'){
-         $scope.cardType = 'Visa';
-         return $scope.cardType;
-       }
-       else if(card[0]+card[1] == '51' || card[0]+card[1] == '52' || card[0]+card[1] == '53' || card[0]+card[1] == '54' ||
-       card[0]+card[1] == '55' || card[0]+card[1] == '22' || card[0]+card[1] == '23' || card[0]+card[1] == '24' || card[0]+card[1] == '25' ||
-       card[0]+card[1] == '26' || card[0]+card[1] == '27'){
-         $scope.cardType = 'Mastercard';
-         return $scope.cardType;
-       }
-       else if(card[0]+card[1] == '34' || card[0]+card[1] == '37'){
-         $scope.cardType = 'AmericanExpress';
-         return $scope.cardType;
-       }
-       else{
-         $scope.cardType = 'Not found';
-         return $scope.cardType;
-       }
+     else if(card[0]+card[1] == '51' || card[0]+card[1] == '52' || card[0]+card[1] == '53' || card[0]+card[1] == '54' ||
+     card[0]+card[1] == '55' || card[0]+card[1] == '22' || card[0]+card[1] == '23' || card[0]+card[1] == '24' || card[0]+card[1] == '25' ||
+     card[0]+card[1] == '26' || card[0]+card[1] == '27'){
+       $scope.cardType = 'Mastercard';
+       return $scope.cardType;
      }
+     else if(card[0]+card[1] == '34' || card[0]+card[1] == '37'){
+       $scope.cardType = 'AmericanExpress';
+       return $scope.cardType;
+     }
+     else{
+       $scope.cardType = 'Not found';
+       return $scope.cardType;
+     }
+   }
 
 }]);
